d386b2074bc42d08b41f69b8143ce986acdb26a7
ccmadrid <kidmadrid@Carls-MacBook-Pro.local>
2014-12-12 00:22:59 -0500

    toggle rough

---
+++ js/returntop.js
@@ -0,0 +1,23 @@
+var speed = 250;
+var fade = 500;
+
+$(document).ready(function(){
+    $(window).bind('scroll', function(){
+        if ($(this).scrollTop() > 150) {
+            $('.rtt').fadeIn(fade);
+        } else {
+            $('.rtt').fadeOut(fade);
+        }
+    });
+});
+
+$(function () {
+    $('body').on("click", ".rtt", function (e) {
+
+        e.preventDefault();
+
+        $('body,html').animate({
+            scrollTop: 0
+        }, speed);
+    });
+});
